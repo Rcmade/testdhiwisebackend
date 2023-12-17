6acace787df5e75169cad4d79e023ee8e3bea39b
Rcmade <rcmade123456@gmail.com>
2023-12-17 16:47:43 +0000

    Code Synced by DhiWise

---
+++ __test__/device/auth.test.js
@@ -0,0 +1,319 @@
+/**
+ * auth.test.js
+ * @description :: contains test cases of APIs for authentication module.
+ */
+
+const dotenv = require('dotenv');
+dotenv.config();
+process.env.NODE_ENV = 'test';
+const db = require('mongoose');
+const request = require('supertest');
+const { MongoClient } = require('mongodb');
+const app = require('../../app');
+const authConstant = require('../../constants/authConstant');
+const uri = 'mongodb://127.0.0.1:27017';
+
+const client = new MongoClient(uri, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true
+});
+
+let insertedUser = {};
+
+/**
+ * @description : model dependencies resolver
+ */
+beforeAll(async function (){
+  try {
+    await client.connect();
+    const dbInstance = client.db('EcomDb_test');
+
+    const user = dbInstance.collection('users');
+    insertedUser = await user.insertOne({
+      username: 'Norval85',
+      password: 'yTrv5GLi0c11KjC',
+      email: 'Jarrell33@hotmail.com',
+      name: 'Andres Auer',
+      shippingAddress: [
+        {
+          _id: false,
+          pincode: 'program',
+          address1: 'red',
+          address2: 'end-to-end',
+          landmark: 'transmitter',
+          city: 'Account',
+          isDefault: false,
+          state: 'Refined',
+          addressType: 'Health',
+          fullName: 'Belarussian',
+          mobile: 810,
+          addressNo: 111
+        }
+      ],
+      wishlist: [ {
+        _id: false,
+        productId: 'hacking' 
+      } ],
+      userType: 24,
+      mobileNo: '(428) 457-6611',
+      loginOTP: {},
+      resetPasswordLink: {},
+      loginRetryLimit: 368,
+      loginReactiveTime: '2024-08-09T21:55:39.108Z',
+      id: '657f25eb2bd25e25af1adcc4'
+    });
+  }
+  catch (error) {
+    console.error(`we encountered ${error}`);
+  }
+  finally {
+    client.close();
+  }
+});
+
+// test cases
+
+describe('POST /register -> if email and username is given', () => {
+  test('should register a user', async () => {
+    let registeredUser = await request(app)
+      .post('/device/auth/register')
+      .send({
+        'username':'Gisselle.Kerluke74',
+        'password':'9XxrurIyRCrewQm',
+        'email':'Elza_McGlynn16@gmail.com',
+        'name':'Cesar Zieme',
+        'shippingAddress':[{
+          '_id':false,
+          'pincode':'navigate',
+          'address1':'Colorado',
+          'address2':'one-to-one',
+          'landmark':'withdrawal',
+          'city':'Gorgeous',
+          'isDefault':true,
+          'state':'withdrawal',
+          'addressType':'deposit',
+          'fullName':'indexing',
+          'mobile':539,
+          'addressNo':713
+        }],
+        'wishlist':[{
+          '_id':false,
+          'productId':'Coordinator'
+        }],
+        'userType':authConstant.USER_TYPES.User,
+        'mobileNo':'(475) 015-1089',
+        'addedBy':insertedUser.insertedId,
+        'updatedBy':insertedUser.insertedId
+      });
+    expect(registeredUser.statusCode).toBe(200);
+    expect(registeredUser.body.status).toBe('SUCCESS');
+    expect(registeredUser.body.data).toMatchObject({ id: expect.any(String) });
+  });
+});
+
+describe('POST /login -> if username and password is correct', () => {
+  test('should return user with authentication token', async () => {
+    let user = await request(app)
+      .post('/device/auth/login')
+      .send(
+        {
+          username: 'Gisselle.Kerluke74',
+          password: '9XxrurIyRCrewQm'
+        }
+      );
+    expect(user.statusCode).toBe(200);
+    expect(user.body.status).toBe('SUCCESS');
+    expect(user.body.data).toMatchObject({
+      id: expect.any(String),
+      token: expect.any(String)
+    }); 
+  });
+});
+
+describe('POST /login -> if username is incorrect', () => {
+  test('should return unauthorized status and user not exists', async () => {
+    let user = await request(app)
+      .post('/device/auth/login')
+      .send(
+        {
+          username: 'wrong.username',
+          password: '9XxrurIyRCrewQm'
+        }
+      );
+
+    expect(user.statusCode).toBe(400);
+    expect(user.body.status).toBe('BAD_REQUEST');
+  });
+});
+
+describe('POST /login -> if password is incorrect', () => {
+  test('should return unauthorized status and incorrect password', async () => {
+    let user = await request(app)
+      .post('/device/auth/login')
+      .send(
+        {
+          username: 'Gisselle.Kerluke74',
+          password: 'wrong@password'
+        }
+      );
+
+    expect(user.statusCode).toBe(400);
+    expect(user.body.status).toBe('BAD_REQUEST');
+  });
+});
+
+describe('POST /login -> if username or password is empty string or has not passed in body', () => {
+  test('should return bad request status and insufficient parameters', async () => {
+    let user = await request(app)
+      .post('/device/auth/login')
+      .send({});
+
+    expect(user.statusCode).toBe(400);
+    expect(user.body.status).toBe('BAD_REQUEST');
+  });
+});
+
+describe('POST /forgot-password -> if email has not passed from request body', () => {
+  test('should return bad request status and insufficient parameters', async () => {
+    let user = await request(app)
+      .post('/device/auth/forgot-password')
+      .send({ email: '' });
+
+    expect(user.statusCode).toBe(400);
+    expect(user.body.status).toBe('BAD_REQUEST');
+  });
+});
+
+describe('POST /forgot-password -> if email passed from request body is not available in database ', () => {
+  test('should return record not found status', async () => {
+    let user = await request(app)
+      .post('/device/auth/forgot-password')
+      .send({ 'email': 'unavailable.email@hotmail.com', });
+
+    expect(user.statusCode).toBe(404);
+    expect(user.body.status).toBe('RECORD_NOT_FOUND');
+  });
+});
+
+describe('POST /forgot-password -> if email passed from request body is valid and OTP sent successfully', () => {
+  test('should return success message', async () => {
+    let user = await request(app)
+      .post('/device/auth/forgot-password')
+      .send({ 'email':'Elza_McGlynn16@gmail.com', });
+
+    expect(user.statusCode).toBe(200);
+    expect(user.body.status).toBe('SUCCESS');
+  });
+});
+
+describe('POST /validate-otp -> OTP is sent in request body and OTP is correct', () => {
+  test('should return success', () => {
+    return request(app)
+      .post('/device/auth/login')
+      .send(
+        {
+          username: 'Gisselle.Kerluke74',
+          password: '9XxrurIyRCrewQm'
+        }).then(login => () => {
+        return request(app)
+          .get(`/device/api/v1/user/${login.body.data.id}`)
+          .set({
+            Accept: 'application/json',
+            Authorization: `Bearer ${login.body.data.token}`
+          }).then(foundUser => {
+            return request(app)
+              .post('/device/auth/validate-otp')
+              .send({ 'otp': foundUser.body.data.resetPasswordLink.code, }).then(user => {
+                expect(user.statusCode).toBe(200);
+                expect(user.body.status).toBe('SUCCESS');
+              });
+          });
+      });
+  });
+});
+
+describe('POST /validate-otp -> if OTP is incorrect or OTP has expired', () => {
+  test('should return invalid OTP', async () => {
+    let user = await request(app)
+      .post('/device/auth/validate-otp')
+      .send({ 'otp': '12334' });
+    
+    expect(user.statusCode).toBe(200);
+    expect(user.body.status).toBe('FAILURE');
+    
+  });
+});
+
+describe('POST /validate-otp -> if request body is empty or OTP has not been sent in body', () => {
+  test('should return insufficient parameter', async () => {
+    let user = await request(app)
+      .post('/device/auth/validate-otp')
+      .send({});
+
+    expect(user.statusCode).toBe(400);
+    expect(user.body.status).toBe('BAD_REQUEST');
+  });
+});
+
+describe('PUT /reset-password -> code is sent in request body and code is correct', () => {
+  test('should return success', () => {
+    return request(app)
+      .post('/device/auth/login')
+      .send(
+        {
+          username: 'Gisselle.Kerluke74',
+          password: '9XxrurIyRCrewQm'
+        }).then(login => () => {
+        return request(app)
+          .get(`/device/api/v1/user/${login.body.data.id}`)
+          .set({
+            Accept: 'application/json',
+            Authorization: `Bearer ${login.body.data.token}`
+          }).then(foundUser => {
+            return request(app)
+              .put('/device/auth/validate-otp')
+              .send({
+                'code': foundUser.body.data.resetPasswordLink.code,
+                'newPassword':'newPassword'
+              }).then(user => {
+                expect(user.statusCode).toBe(200);
+                expect(user.body.status).toBe('SUCCESS');
+              });
+          });
+      });
+  });
+});
+
+describe('PUT /reset-password -> if request body is empty or code/newPassword is not given', () => {
+  test('should return insufficient parameter', async () => {
+    let user = await request(app)
+      .put('/device/auth/reset-password')
+      .send({});
+    
+    expect(user.statusCode).toBe(400);
+    expect(user.body.status).toBe('BAD_REQUEST');
+  });
+});
+
+describe('PUT /reset-password -> if code is invalid', () => {
+  test('should return invalid code', async () => {
+    let user = await request(app)
+      .put('/device/auth/reset-password')
+      .send({
+        'code': '123',
+        'newPassword': 'testPassword'
+      });
+
+    expect(user.statusCode).toBe(200);
+    expect(user.body.status).toBe('FAILURE');
+
+  });
+});
+
+afterAll(function (done) {
+  db.connection.db.dropDatabase(function () {
+    db.connection.close(function () {
+      done();
+    });
+  });
+});
